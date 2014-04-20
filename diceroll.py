72369382ab2de596961348e96a4ddb1b1dd3c660
grimgamer <isaac@monkeez.org>
2014-04-20 15:30:00 +0100

    Update diceroll.py

--- diceroll.py
+++ diceroll.py
@@ -1,4 +1,2 @@
-Python 3.4.0 (default, Mar 17 2014, 23:20:09) 
-[GCC 4.8.2 20140206 (prerelease)] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> 
+
+>>> print "lolololololololol"
